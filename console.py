320d6f94af36b5a62c6ee0cc15b68f8178098e34
pnrmhcl <pmihcioglu@gmail.com>
2022-12-19 15:59:27 +0300

    fix: hour and minute detail

--- console.py
+++ console.py
@@ -5,14 +5,16 @@ wb = load_workbook("phone.xlsx")
 ws = wb.active
 print(ws)
 # saat ve dakikayı başladığınız zaman dilimine göre ayarlamalısınız.
-hour = 14
-minute = 45
+
+
 for satir in range(1, ws.max_row + 1):
+    hour = 15
+    minute = 0
     print(str(ws.cell(satir, 1).value), end="")
     print()
     try:
-        pywhatkit.sendwhatmsg(str(ws.cell(satir, 1).value), "Bu mesaj Pınar Mıhcıoğlu tarafından gönderilmiştir.", hour,
-                              minute + 10)
+        pywhatkit.sendwhatmsg(str(ws.cell(satir, 1).value), "Bu mesaj Pınar Mıhcıoğlu tarafından gönderilmiştir.",
+                              hour + 1,
+                              minute)
     except Exception as e:
-
         print("hata oluştu", e)
